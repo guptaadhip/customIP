d7609967c50b4d80b46c5e4cef62d4353a03d0d4
Adhip Gupta <adhipgup@usc.edu>
2014-09-27 16:48:43 -0700

    sendPing function definition added

--- include/PacketEngine.h
+++ include/PacketEngine.h
@@ -7,6 +7,7 @@ class PacketEngine {
  	PacketEngine();
   void forwardPacket(const u_char *packet, uint32_t nextHopAddr);
 	void responsePacket(const u_char *packet, IcmpResponse type);
+	void sendPing(const uint32_t srcAddr, const uint32_t dstAddr);
 	
  private:
   unsigned short checksum(unsigned short *addr, int len);
