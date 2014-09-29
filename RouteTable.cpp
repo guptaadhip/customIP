96f79311ce58f3a988dcf4e81dba9d2e42dd9052
Apoorv Bhargava <apoorvbhargava89@gmail.com>
2014-09-28 22:34:42 -0700

    Removed Error from last commit

--- RouteTable.cpp
+++ RouteTable.cpp
@@ -59,14 +59,14 @@ void RouteTable::remove(uint32_t address) {
 void RouteTable::printRouteTable() {
 	for (auto entry : routeTable_) {
 		auto temp = entry.second;
-		struct sockaddr_in *sa = (struct sockaddr_in *) temp.getNwAddress();
+		struct sockaddr_in  *sa = (struct sockaddr_in *) temp.getNwAddress();
 		char *addr = inet_ntoa(sa->sin_addr);
 		
-		struct sockaddr_in *sa = (struct sockaddr_in *) temp.getNextHop();
-		char *hop = inet_ntoa(sa->sin_addr);
+		struct sockaddr_in  *sai = (struct sockaddr_in *) temp.getNextHop();
+		char *hop = inet_ntoa(sai->sin_addr);
 		
-		struct sockaddr_in *sa = (struct sockaddr_in *) temp.getSubnetMask();
-		char *subnet = inet_ntoa(sa->sin_addr);
+		struct sockaddr_in  *sal = (struct sockaddr_in *) temp.getSubnetMask();
+		char *subnet = inet_ntoa(sal->sin_addr);
 		
 		cout << temp.getNwAddress() << " " << addr << " " << temp.getNextHop() 
 				 << " " << hop << " " << temp.getSubnetMask() << " " << subnet << " " << temp.getInterface()
